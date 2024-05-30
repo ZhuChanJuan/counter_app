aac1d5d061644707a8602d2cd55597398aed3943
Vicky Zhu <vicky.zhu@prudential.com.cn>
2024-05-30 10:51:52 +0800

    Committing changes to test runner scripts

--- integration_test/run_all_tests.sh
+++ integration_test/run_all_tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+set -e
+cd /Users/vickyzhu/FlutterProjects/counter_app/integration_test
 # Define the folder containing your test files
-TEST_FOLDER="homepage"
+# TEST_FOLDER="homepage"
 # TEST_FOLDER="."
 
-# Define the feature file to be executed first (if the file not in the TEST_FOLDER, then we need change script)
-# FEATURE_FILE="login_test_test.dart"
-# Define the test files
-first_file="login_test_test.dart"
 
 # # Check if the test folder is provided
 # if [ -z "$TEST_FOLDER" ]; then
@@ -21,36 +19,12 @@ first_file="login_test_test.dart"
 #     exit 1
 # fi
 
-# Find all Dart test files in the specified folder
-all_test_files=$(find $TEST_FOLDER -name "*_test.dart")
 
-# Execute the feature file first
-feature_file_path="$TEST_FOLDER/$first_file"
-if [ -f "$feature_file_path" ]; then
-    echo "Adding feature file: $feature_file_path"
-    target_option=" --target=integration_test/$feature_file_path"
-fi
-
-# Append remaining test files to the list
-for test_file in $all_test_files
-do
-    # Skip login_test_test.dart since it's already added
-    if [ "$test_file" == "$feature_file_path" ]; then
-        continue
-    fi
-
-    # Append the test file to the list
-    target_option+=" --target=integration_test/$test_file"
+# Find and run all *_test.dart files in the root folder using flutter drive
+for test_file in $(find . -maxdepth 1 -type f -name "*_test.dart"); do
+    test_file=${test_file#./}
+    echo "Running test: $test_file"
+    flutter drive --driver=integration_test/integration_driver.dart -t integration_test/"$test_file" -d chrome
 done
-
-
-# Execute the flutter drive command with the constructed --target option
-echo "Running tests: $target_option"
-cd ..
-flutter drive --driver=integration_test/integration_driver.dart $target_option -d chrome
-if [ $? -ne 0 ]; then
-    echo "Tests failed."
-    exit 1
-fi
 
 echo "All tests passed!"
--- integration_test/run_all_tests_parallel.sh
+++ integration_test/run_all_tests_parallel.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+set -e
+cd /Users/vickyzhu/FlutterProjects/counter_app/integration_test
 
 # Define the folder containing your test files
-TEST_FOLDER="integration_test"
+# TEST_FOLDER="."
 
 # Find all Dart test files in the specified folder
-test_files=$(find $TEST_FOLDER -name "*_test.dart")
-
-# Export necessary environment variables for flutter command
-export PATH="$PATH:$(flutter sdk-path)/bin"
+test_files=$(find . -maxdepth 1 -type f -name "*_test.dart")
 
 # Function to run a single test file
 run_test() {
     local test_file=$1
+    # echo "Running test: ${test_file#./}"
+    test_file=${test_file#./}
     echo "Running test: $test_file"
-    flutter drive --target=$test_file
+    flutter drive --driver=integration_test/integration_driver.dart -t integration_test/"$test_file" -d chrome
     if [ $? -ne 0 ]; then
         echo "Test failed: $test_file"
         exit 1
@@ -23,7 +24,7 @@ run_test() {
 export -f run_test
 
 # Run tests in parallel
-echo "$test_files" | parallel -j $(nproc) run_test {}
+echo "$test_files" | parallel -j 5 run_test {}
 
 if [ $? -eq 0 ]; then
     echo "All tests passed!"
