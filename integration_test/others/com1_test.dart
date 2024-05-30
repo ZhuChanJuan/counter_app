5beb3c692e10df09191d8023331656b5785e0f06
Vicky Zhu <vicky.zhu@prudential.com.cn>
2024-05-30 10:54:03 +0800

    move other test file to others folder

--- integration_test/others/com1_test.dart
+++ integration_test/others/com1_test.dart
@@ -1,5 +1,5 @@
-import 'login_test_test.dart' as test1;
-import 'counter_test.dart' as test2;
+import '../login_test_test.dart' as test1;
+import '../counter_test.dart' as test2;
 
 // i tried with command "flutter drive --driver=test_driver/integration_test.dart  -t integration_test/com1_test.dart -d chrome " to run the combination test suite , the result :
 // the app will go back to the homepage after first test case finished, so we still need add initialization code for other test scenarios
